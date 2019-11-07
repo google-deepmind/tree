c1b16cac4c955bbc437f45b6bac387c50b9f8d7e
Sergei Lebedev <slebedev@google.com>
2019-11-07 08:08:19 -0800

    Release 0.1.1
    PiperOrigin-RevId: 279088703
    Change-Id: I16aadc2b9ee4d5496341d58406ff3107e07266b6

--- docs/changes.rst
+++ docs/changes.rst
@@ -2,6 +2,13 @@
 Changelog
 #########
 
+Version 0.1.1
+=============
+
+Released 2019-11-07
+
+* Ensured that the produced Linux wheels are manylinux2010-compatible.
+
 Version 0.1.0
 =============
 
--- tree/__init__.py
+++ tree/__init__.py
@@ -48,7 +48,7 @@ __all__ = [
     "map_structure_with_path_up_to",
 ]
 
-__version__ = "0.1.0"
+__version__ = "0.1.1"
 
 # Note: this is *not* the same as `six.string_types`, which in Python3 is just
 #       `(str,)` (i.e. it does not include byte strings).
