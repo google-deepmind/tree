623f38ef23d9953a44a1f4a8dae56e889e1502db
Sergei Lebedev <slebedev@google.com>
2021-03-25 06:57:46 -0700

    Added Python 3.9 to the CI matrix and setup.py
    PiperOrigin-RevId: 365027159

--- setup.py
+++ setup.py
@@ -142,6 +142,7 @@ setuptools.setup(
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
         'Topic :: Scientific/Engineering :: Mathematics',
         'Topic :: Software Development :: Libraries',
     ],
