a7af5bd5ab0fef274a8cf4ee6a90359a0bedd5ce
Sergei Lebedev <slebedev@google.com>
2022-12-17 14:58:50 -0800

    Dropped 3.6 from the list of supported Python versions
    This commit also upgrades the CI environment to manylinux2014, because manylinux2010
    is no longer supported. See pypa/manylinux#1281.
    
    PiperOrigin-RevId: 496128466

--- setup.py
+++ setup.py
@@ -143,7 +143,6 @@ setuptools.setup(
         'Intended Audience :: Developers',
         'Intended Audience :: Science/Research',
         'License :: OSI Approved :: Apache Software License',
-        'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
